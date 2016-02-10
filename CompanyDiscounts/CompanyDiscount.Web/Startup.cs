14bca6019bb0c5fbb8df212517abb4d9673de4db
Margarita Ivancheva <mvivancheva@gmail.com>
2016-02-10 12:11:31 +0200

    Added initial projects architecture

---
+++ CompanyDiscounts/CompanyDiscount.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompanyDiscount.Web.Startup))]
+namespace CompanyDiscount.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
