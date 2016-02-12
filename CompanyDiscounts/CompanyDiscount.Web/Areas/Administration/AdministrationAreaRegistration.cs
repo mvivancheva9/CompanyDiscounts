0594f282c405bde1afa1fe0bbaf8fda47669b487
Margarita Ivancheva <mvivancheva@gmail.com>
2016-02-12 17:24:24 +0200

    Added initial Admin panel design

--- CompanyDiscounts/CompanyDiscount.Web/Areas/Administration/AdministrationAreaRegistration.cs
+++ CompanyDiscounts/CompanyDiscount.Web/Areas/Administration/AdministrationAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace CompanyDiscount.Web.Areas.Administration
             context.MapRoute(
                 "Administration_default",
                 "Administration/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
