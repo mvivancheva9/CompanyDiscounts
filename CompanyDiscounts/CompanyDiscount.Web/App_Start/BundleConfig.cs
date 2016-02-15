d7e1e25b5503b8be0e639cc7f3e96e331d86f53f
Margarita Ivancheva <mvivancheva@gmail.com>
2016-02-15 15:59:30 +0200

    extracted styles and scripts to bundle

--- CompanyDiscounts/CompanyDiscount.Web/App_Start/BundleConfig.cs
+++ CompanyDiscounts/CompanyDiscount.Web/App_Start/BundleConfig.cs
@@ -27,9 +27,34 @@ namespace CompanyDiscount.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                     "~/Scripts/AdministrationScripts/bootstrap.min.js",
+                     "~/Scripts/AdministrationScripts/Chart.js",
+                     "~/Scripts/AdministrationScripts/classie.js",
+                     "~/Scripts/AdministrationScripts/jquery-1.10.2.min.js",
+                     "~/Scripts/AdministrationScripts/jquery.flexisel.js",
+                     "~/Scripts/AdministrationScripts/jquery.flot.min.js",
+                     "~/Scripts/AdministrationScripts/jquery.nicescroll.js",
+                     "~/Scripts/AdministrationScripts/scripts.js",
+                     "~/Scripts/AdministrationScripts/skycons.js",
+                     "~/Scripts/AdministrationScripts/uisearch.js",
+                     "~/Scripts/AdministrationScripts/wow.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/admin").Include(
+                "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/animate.css",
+                      "~/Content/css/colorbox.css",
+                      "~/Content/css/elastislide.css",
+                      "~/Content/css/font-awesome.css",
+                      "~/Content/css/graph.css",
+                      "~/Content/css/icon-font.min.css",
+                      "~/Content/css/magnific-popup.css",
+                      "~/Content/css/style.css"
+                      ));
         }
     }
 }
